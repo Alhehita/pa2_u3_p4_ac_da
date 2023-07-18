a02df9815d563a43b90146116b38f35219808d6b
Dennisse Andrade <daandradem1@uce.edu.ec>
2023-07-18 12:10:13 -0500

    Actualizacion

--- src/main/java/com/example/demo/Pa2U3P4AcDaApplication.java
+++ src/main/java/com/example/demo/Pa2U3P4AcDaApplication.java
@@ -9,13 +9,29 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.repository.modelo.Estudiante;
 import com.example.demo.repository.modelo.Habitacion;
 import com.example.demo.repository.modelo.Hotel;
+import com.example.demo.repository.modelo.Materia;
+import com.example.demo.repository.modelo.Matricula;
+import com.example.demo.repository.modelo.Provincia;
+import com.example.demo.repository.modelo.Semestre;
+import com.example.demo.service.EstudianteService;
 import com.example.demo.service.HotelService;
+import com.example.demo.service.MateriaService;
+import com.example.demo.service.MatriculaService;
 
 @SpringBootApplication
 public class Pa2U3P4AcDaApplication implements CommandLineRunner {
 
+	@Autowired
+	private EstudianteService estudianteService;
+	
+	@Autowired
+	private MatriculaService matriculaService;
+	
+	@Autowired
+	private MateriaService materiaService;
 	
 
 	public static void main(String[] args) {
@@ -26,7 +42,24 @@ public class Pa2U3P4AcDaApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 
+		Semestre semestre = new Semestre();
+		Provincia provincia = new Provincia();
 		
+		Estudiante estudiante = new Estudiante();
+		Matricula matricula = new Matricula();
+		Materia materia = new Materia();
+		
+		semestre.setNumero("6");
+		semestre.setNumMaterias("5");
+		semestre.setPeriodo("2023-2023");
+		
+		provincia.setNombre("Pichincha");
+		provincia.setEstudiantes(null);
+		provincia.setCapital("Quito");
+		
+		List<Materia> materias = new ArrayList<>();
+		
+		semestre.setMaterias(materias);
 			
 		}
 		
